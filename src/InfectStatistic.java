97f6e4c8a555b3c577ed5590bdac7f4bda766987
muxiao167 <60741631+muxiao167@users.noreply.github.com>
2020-02-09 20:47:30 +0800

    Update InfectStatistic.java
    新功能：能够正确读入log日志文件，并统计全国以及各省份的各种类型病患人数。

--- src/InfectStatistic.java
+++ src/InfectStatistic.java
@@ -7,6 +7,11 @@
  * @since 2020-02-06
  */
 
+import java.io.File;
+import java.io.*;
+import java.util.List;
+import java.util.regex.*;
+
 class InfectStatistic {
 	
     public static void main(String[] args) {
@@ -20,7 +25,10 @@ class InfectStatistic {
     		System.out.println("命令行格式有误");
     		return;
     	}
-    	
+    	FileHandle filehandle = new FileHandle(cmdargs.log_path,
+    			cmdargs.out_path,cmdargs.date,cmdargs.type,cmdargs.province);
+    	filehandle.getFileList();
+    	filehandle.writeTxt();
     }
 }
 
@@ -31,20 +39,21 @@ class CmdArgs{
 	//int i; //将args的下表位置定义为全局变量，便于传递
 	String[] args; //保存传入的命令行
 	
-	String log_path; //日志文件位置
-	String out_path; //输出文件位置
-	String date; //指定日期
+	public String log_path; //日志文件位置
+	public String out_path; //输出文件位置
+	public String date; //指定日期
 	
 	/*
 	 * 指定类型(按顺序分别为ip,sp,cure,dead)
 	 * 当数值为1时表示需要列出，为0时无需列出
 	 */
-	int[] type = {1,1,1,1};
+	public int[] type = {1,1,1,1};
 	
 	/*
 	 * 指定省份(按省份名称排序（第一位为全国）)
+	 * 当数值为1时表示需要列出，为0时无需列出
 	 */
-	int province[] = new int[35];
+	public int[] province = new int[35];
 	
 	CmdArgs(String[] args_str){
 		args = args_str;
@@ -180,4 +189,288 @@ class CmdArgs{
 		return i;
 	}
 	
+}
+
+
+/*
+ * 处理文件（读取目录下的文件，打开文件，读取内容，输出内容到文件）
+ */
+class FileHandle{
+	public String log_path; //日志文件位置
+	public String out_path; //输出文件位置
+	public String date; //指定日期
+	
+	/*
+	 * 指定类型(按顺序分别为ip,sp,cure,dead)
+	 * 当数值为1时表示需要列出，为0时无需列出
+	 */
+	public int[] type = {1,1,1,1};
+	
+	/*
+	 * 指定省份(按省份名称拼音排序（第一位为全国）)
+	 * 当数值为1时表示需要列出，为0时无需列出
+	 */
+	public int[] province = new int[35];
+	public String[] fileName = new String[9]; //存储指定日期下的文件名
+	
+	/*
+	 * 省份列表(按省份名称拼音排序（第一位为全国）)
+	 */
+	public String[] province_str = {"全国", "安徽", "澳门" ,"北京", "重庆", "福建","甘肃",
+			"广东", "广西", "贵州", "海南", "河北", "河南", "黑龙江", "湖北", "湖南", "吉林",
+			"江苏", "江西", "辽宁", "内蒙古", "宁夏", "青海", "山东", "山西", "陕西", "上海",
+			"四川", "台湾", "天津", "西藏", "香港", "新疆", "云南", "浙江"};
+	/*
+	 * 人数统计(一维代表省份排序，二维表示类型排序)
+	 * 按省份名称拼音排序（第一位为全国）
+	 * 类型按顺序分别为ip,sp,cure,dead
+	 */
+	public int[][] people = new int[35][4]; 
+	
+	FileHandle(String log, String out, String d, int[] t, int[] p) {
+		log_path = log;
+		out_path = out;
+		date = d + ".log.txt"; //日志文件名后有.log.txt，在此加上便于比较
+		type = t;
+		province = p;
+	}
+	
+	/*
+	 * 读取指定路径下的文件名和目录名
+	 */
+	public void getFileList() {
+		File file = new File(log_path);
+		File[] fileList = file.listFiles();
+		String fileName;
+ 
+		for (int i = 0; i < fileList.length; i++) {
+			fileName = fileList[i].getName();
+			if (fileName.compareTo(date) <= 0) {
+				readTxt(log_path + "\\" + fileName);
+			}
+		}
+	}
+	
+	
+	/*
+	 * 读取文件内容
+	 * @param filePath
+	 */
+    public void readTxt(String filePath) {
+        try {
+            BufferedReader bfr = new BufferedReader(new InputStreamReader(
+            		new FileInputStream(new File(filePath)), "UTF-8"));
+            String lineTxt = null;
+            
+            while ((lineTxt = bfr.readLine()) != null) {
+            	if(!lineTxt.startsWith("//"))
+            		textProcessing(lineTxt);
+            }
+            bfr.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /*
+     * 文本处理
+     * @param string
+     */
+    public void textProcessing(String string) {
+    	String pattern1 = "(\\S+) 新增 感染患者 (\\d+)人";
+    	String pattern2 = "(\\S+) 新增 疑似患者 (\\d+)人";
+    	String pattern3 = "(\\S+) 治愈 (\\d+)人";
+    	String pattern4 = "(\\S+) 死亡 (\\d+)人";
+    	String pattern5 = "(\\S+) 感染患者 流入 (\\S+) (\\d+)人";
+    	String pattern6 = "(\\S+) 疑似患者 流入 (\\S+) (\\d+)人";
+    	String pattern7 = "(\\S+) 疑似患者 确诊感染 (\\d+)人";
+    	String pattern8 = "(\\S+) 排除 疑似患者 (\\d+)人";
+    	boolean isMatch1 = Pattern.matches(pattern1, string);
+    	boolean isMatch2 = Pattern.matches(pattern2, string);
+    	boolean isMatch3 = Pattern.matches(pattern3, string);
+    	boolean isMatch4 = Pattern.matches(pattern4, string);
+    	boolean isMatch5 = Pattern.matches(pattern5, string);
+    	boolean isMatch6 = Pattern.matches(pattern6, string);
+    	boolean isMatch7 = Pattern.matches(pattern7, string);
+    	boolean isMatch8 = Pattern.matches(pattern8, string);
+    	
+    	if(isMatch1) { //新增 感染患者处理
+    		addIP(string);
+    	} else if(isMatch2){ //新增 疑似患者处理
+    		addSP(string);
+    	} else if(isMatch3){ //新增 治愈患者处理
+    		addCure(string);
+    	} else if(isMatch4){ //新增 死亡患者处理
+    		addDead(string);
+    	} else if(isMatch5){ //感染患者 流入处理
+    		flowIP(string);
+    	} else if(isMatch6){ //疑似患者 流入处理
+    		flowSP(string);
+    	} else if(isMatch7){ //疑似患者 确诊感染处理
+    		diagnosisSP(string);
+    	} else if(isMatch8){ //排除 疑似患者处理
+    		removeSP(string);
+    	}
+    }
+    
+    /*
+     * 新增 感染患者处理
+     * @param string
+     */
+    public void addIP(String string) {
+    	String[] str_arr = string.split(" "); //将字符串以空格分割为多个字符串
+    	int i;
+    	int n = Integer.valueOf(str_arr[3].replace("人", ""));//将人前的数字从字符串类型转化为int类型
+    	
+    	for(i = 0; i < province_str.length; i++) {
+    		if(str_arr[0].equals(province_str[i])) { //第一个字符串为省份
+    			people[0][0] += n; //全国感染患者人数增加
+    			people[i][0] += n; //该省份感染患者人数增加
+    			break;
+    		}
+    	}
+    }
+    
+    /*
+     * 新增 疑似患者处理
+     * @param string
+     */
+    public void addSP(String string) {
+    	String[] str_arr = string.split(" "); //将字符串以空格分割为多个字符串
+    	int i;
+    	int n = Integer.valueOf(str_arr[3].replace("人", ""));//将人前的数字从字符串类型转化为int类型
+    	
+    	for(i = 0; i < province_str.length; i++) {
+    		if(str_arr[0].equals(province_str[i])) { //第一个字符串为省份
+    			people[0][1] += n; //全国疑似患者人数增加
+    			people[i][1] += n; //该省份疑似患者人数增加
+    			break;
+    		}
+    	}
+    }
+    
+    /*
+     * 新增 治愈患者处理
+     * @param string
+     */
+    public void addCure(String string) {
+    	String[] str_arr = string.split(" "); //将字符串以空格分割为多个字符串
+    	int i;
+    	int n = Integer.valueOf(str_arr[2].replace("人", ""));//将人前的数字从字符串类型转化为int类型
+    	
+    	for(i = 0; i < province_str.length; i++) { 
+    		if(str_arr[0].equals(province_str[i])) { //第一个字符串为省份
+    			people[0][2] += n; //全国治愈人数增加
+    			people[0][0] -= n; //全国感染患者人数减少
+    			people[i][2] += n; //该省份治愈人数增加
+    			people[i][0] -= n; //该省份感染患者人数减少
+    			break;
+    		}
+    	}
+    }
+    
+    /*
+     * 新增 死亡患者处理
+     * @param string
+     */
+    public void addDead(String string) {
+    	String[] str_arr = string.split(" "); //将字符串以空格分割为多个字符串
+    	int i;
+    	int n = Integer.valueOf(str_arr[2].replace("人", ""));//将人前的数字从字符串类型转化为int类型
+    	
+    	for(i = 0; i < province_str.length; i++) {
+    		if(str_arr[0].equals(province_str[i])) { //第一个字符串为省份
+    			people[0][3] += n; //全国死亡人数增加
+    			people[0][0] -= n; //全国感染患者人数减少
+    			people[i][3] += n; //该省份死亡人数增加
+    			people[i][0] -= n; //该省份感染患者人数减少
+    			break;
+    		}
+    	}
+    }
+    
+    /*
+     * 感染患者 流入处理
+     * @param string
+     */
+    public void flowIP(String string) {
+    	String[] str_arr = string.split(" "); //将字符串以空格分割为多个字符串
+    	int i;
+    	int n = Integer.valueOf(str_arr[4].replace("人", ""));//将人前的数字从字符串类型转化为int类型
+    	
+    	for(i = 0; i < province_str.length; i++) {
+    		if(str_arr[0].equals(province_str[i])) //第一个字符串为流出省份
+    			people[i][0] -= n; //该省份感染患者人数减少
+    		if(str_arr[3].equals(province_str[i])) //第四个字符串为流入省份
+    			people[i][0] += n; //该省份感染患者人数增加
+    	}
+    }
+    
+    /*
+     * 疑似患者 流入处理
+     * @param string
+     */
+    public void flowSP(String string) {
+    	String[] str_arr = string.split(" "); //将字符串以空格分割为多个字符串
+    	int i;
+    	int n = Integer.valueOf(str_arr[4].replace("人", ""));//将人前的数字从字符串类型转化为int类型
+    	
+    	for(i = 0; i < province_str.length; i++) {
+    		if(str_arr[0].equals(province_str[i])) //第一个字符串为流出省份
+    			people[i][1] -= n; //该省份疑似患者减少
+    		if(str_arr[3].equals(province_str[i])) //第四个字符串为流入省份
+    			people[i][1] += n; //该省份疑似患者增加
+    	}
+    	
+    }
+    
+    /*
+     * 疑似患者 确诊感染处理
+     * @param string
+     */
+    public void diagnosisSP(String string) {
+    	String[] str_arr = string.split(" "); //将字符串以空格分割为多个字符串
+    	int i;
+    	int n = Integer.valueOf(str_arr[3].replace("人", ""));//将人前的数字从字符串类型转化为int类型
+    	
+    	for(i = 0; i < province_str.length; i++) {
+    		if(str_arr[0].equals(province_str[i])) { //第一个字符串为省份
+    			people[0][1] -= n; //全国疑似患者人数减少
+    			people[0][0] += n; //全国感染患者人数增加
+    			people[i][1] -= n; //该省份疑似患者人数减少
+    			people[i][0] += n; //该省份感染患者人数增加
+    			break;
+    		}
+    	}
+    }
+    
+    /*
+     * 排除 疑似患者处理
+     * @param string
+     */
+    public void removeSP(String string) {
+    	String[] str_arr = string.split(" "); //将字符串以空格分割为多个字符串
+    	int i;
+    	int n = Integer.valueOf(str_arr[3].replace("人", ""));//将人前的数字从字符串类型转化为int类型
+    	
+    	for(i = 0; i < province_str.length; i++) {
+    		if(str_arr[0].equals(province_str[i])) { //第一个字符串为省份
+    			people[i][1] -= n; //该省份疑似患者人数减少
+    			people[0][1] -= n; //全国疑似患者人数减少
+    			break;
+    		}
+    	}
+    }
+    
+    
+    /*
+	 * 输出文件内容
+	 */
+    public void writeTxt() {
+        try {
+        	
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
